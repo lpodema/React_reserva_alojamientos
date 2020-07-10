341d21f29413748fe450ac31cfcfc3074fc0755c
Emanuel <emanuelporres@hotmail.com>
2020-07-10 20:49:25 -0300

    functional con error

--- scripts/components/HotelContainer.js
+++ scripts/components/HotelContainer.js
@@ -1,22 +1,26 @@
 function HotelContainer(props) {
     const { hoteles } = props;
     console.log(hoteles);
-    return <div className='hotel-container'></div>;
+    return (
+        <div className='hotel-container'>
+            {this.props.hoteles.map((hotel) => {
+                return (
+                    <Card
+                        key={hotel.slug}
+                        name={hotel.name}
+                        photo={hotel.photo}
+                        description={hotel.description}
+                        rooms={hotel.rooms}
+                        city={hotel.city}
+                        country={hotel.country}
+                        price={hotel.price}
+                    />
+                );
+            })}
+        </div>
+    );
 }
 
 /*
-{this.props.hoteles.map((hotel) => {
-    return (
-        <Card
-            key={hotel.slug}
-            name={hotel.name}
-            photo={hotel.photo}
-            description={hotel.description}
-            rooms={hotel.rooms}
-            city={hotel.city}
-            country={hotel.country}
-            price={hotel.price}
-        />
-    );
-})}
+
 */
