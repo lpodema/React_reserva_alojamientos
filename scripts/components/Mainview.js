76e738a1e10f71230e076f8d94733c629e436417
Emanuel <emanuelporres@hotmail.com>
2020-07-18 19:40:18 -0300

    Responsive terminado

--- scripts/components/Mainview.js
+++ scripts/components/Mainview.js
@@ -1,8 +1,14 @@
 function Mainview(props) {
+    const scrollToTop = () => {
+        window.scrollTo(0, 0);
+    };
     const { hoteles } = props;
     return (
         <div className='mainview'>
             <HotelContainer hoteles={hoteles} filterData={props.filterData} />
+            <button onClick={scrollToTop} className='toTheTop'>
+                <i className='fa fa-chevron-up' aria-hidden='true'></i>
+            </button>
         </div>
     );
 }
